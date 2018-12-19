1ced67735fa65de4a969557d62935d2b1558b3b7
Divinaa Burder <dburder@lasp.colorado.edu>
2018-12-19 14:50:01 -0700

    updated Sout to EKF

--- EKF.m
+++ EKF.m
@@ -1,4 +1,4 @@
-function [xp, P, ey, S_out] = EKF(C, xp, P, times, msrs, Q, Rmat)
+function [xp, P, nis, S_out] = EKF(C, xp, P, times, msrs, Q, Rmat)
 t_span = [0, 10];
 z = size(times, 2);
 ycount = 3;
@@ -13,33 +13,42 @@ for k = 1:length(msrs)
     
     % Prediction
     F = F_tilde(C, times(k));
-    [~, out_states] = ode45(@(t, x) Full_Nonlinear_Dynamics(C, t, x, w_k), t_span, xp(:, k)); 
+    opts = odeset('RelTol', 1e-9, 'AbsTol', 1e-10);
+    [~, out_states] = ode45(@(t, x) Full_Nonlinear_Dynamics(C, t, x, w_k), t_span, xp(:, k)', opts);
     xm = out_states(end,:)'; 
     Pm = F * P(:, :, k) * F'+ Q;
+    
+    if isnan(xm)
+        break
+    end 
     
     for stn_num = 1:12
         j = 1:3:36;
         i = j(stn_num);
         if ~isnan(msrs(i, k))
-            pred_msrs = [pred_msrs; True_Msrs(C, xm, times(k), stn_num)];
+            pred_msrs = [pred_msrs; True_Msrs(C, xm', times(k +1), stn_num)]
             msrs_block = [msrs_block; msrs(i:i+2, k)]; 
             H_block = [H_block; H_i(C, times(k), stn_num)];
             R_block = blkdiag(R_block, Rmat); 
         end
     end
-
+    nis(k) = nan(1); 
     if ~isempty(msrs_block)
         % Correction
         S = (H_block * Pm * H_block' + R_block);
         K = Pm * H_block' / S;
-        ey{:, k+1} = msrs_block - pred_msrs;
-        xp(:, k+1) = xm + K * ey{:, k+1};
+        ey = msrs_block - pred_msrs;
+        xp(:, k+1) = xm + K * ey;
         P(:, :, k+1) = (eye(4) - K * H_block) * Pm;
+        nis(k) = ey' * inv(S) * ey;
     else
         xp(:, k+1) = xm;
         P(:, :, k+1) = Pm;
+        S = [];
     end
+    S_out{k} = S;
+    
 end
 
-S_out(:,:,k+1) = S;
+
 end
