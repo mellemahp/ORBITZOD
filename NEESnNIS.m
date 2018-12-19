254f509c0973a3ebbda15e7149cf62206d24ba81
Divinaa Burder <dburder@lasp.colorado.edu>
2018-12-18 22:11:54 -0700

    Nees and Nis

--- NEESnNIS.m
+++ NEESnNIS.m
@@ -1,26 +1,24 @@
-function [ex, ey, mu_ex, mu_ey] = NEESnNIS(ex, ey, P, S, Obs)
+function [ex, ey, mu_ex, mu_ey] = NEESnNIS(ex, ey, P, S, msrs)
 
 count = size(P,4);
 xcount = size(P,1);
 ycount = size(S,1);
 z = size(ex, 2);
 
-ex = zeros(1, z, count);
 mu_ex = zeros(1, z);
 mu_ey = zeros(1, z);
 for i = 1:count
    
-    for s = 1:z
-        ex(1, s, i) = (ex(:, s, i)' / P(:, :, s, i)) * ex(:, s, i); 
-        ey(1, s, i) = ey(:, s, i)' / S(:, :, s, i) * ey(:, s, i);
-    end
+    epsilonx(:, i) = (ex(:, i)' * inv( P(:, :, i))) * ex(:,i); 
+    epsilony(:, i) = ey{:, i}' * inv(S(:, :, i)) * ey{:, i};
+
 end
 
 for s = 1:z
-        mu_ex(1,s) = mean(ex(1,s,:));
-        mu_ey(1,s) = mean(ey(1,s,:));
+        mu_ex(1,s) = mean(epsilonx(:,s));
+        mu_ey(1,s) = mean(epsilony(:,s));
 end
 
-mu_ex(Obs == 0) = nan;
+mu_ex(msrs == 0) = nan;
 
 end
