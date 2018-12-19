d47ed78d3f204e779402d9b114b81fc14f2f1c04
Divinaa Burder <dburder@lasp.colorado.edu>
2018-12-19 14:49:12 -0700

    updated NEES and Nis in kf

--- kalman_filter/Kalman_Filter.m
+++ kalman_filter/Kalman_Filter.m
@@ -1,4 +1,4 @@
-function [P, xp] = Kalman_Filter(C, times, msrs, xp, P, Rmat, Q) 
+function [P, xp, S_log, H_block, msrs_sansNaN] = Kalman_Filter(C, times, msrs, xp, P, Rmat, Q) 
     % Runs a Kalman Filter on a set of measurements
     for t = 1:length(msrs)
         residuals_bloc = [];
@@ -12,11 +12,15 @@ function [P, xp] = Kalman_Filter(C, times, msrs, xp, P, Rmat, Q)
                 residuals_bloc = [residuals_bloc; resid_t]; 
                 H_block = [H_block; H_i(C, times(t), stn_num)];
                 R_block = blkdiag(R_block, Rmat); 
+                msrs_sansNaN(:,t) = msrs(i:i + 2, t);
+                
             end
         end
         F = F_tilde(C, t);
         if ~isempty(residuals_bloc)
             [P(:,:,t+1), xp(:,t+1)] = Kalman_Step(F, H_block, xp(:,t), P(:,:,t), residuals_bloc, Q, R_block);
+            S = (H_block(1:3,1:4) * P(:,:,t) * H_block(1:3,1:4)' + R_block(1:3,1:3));
+            S_log{t} = S;
         else 
             [P(:,:,t+1), xp(:,t+1)] = Pure_Pred_Step(F, xp(:,t), P(:,:,t), Q);
         end
